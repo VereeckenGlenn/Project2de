5a0e8802d0c4077c8d24af934fd9526915227165
glenn <glenn@172.18.133.36>
2019-03-15 12:14:12 +0100

    domein class tegels geimplementeerd

--- src/domein/Tegel.java
+++ src/domein/Tegel.java
@@ -7,8 +7,41 @@ package domein;
 
 /**
  *
- * @author loren
+ * @author Glenn
  */
 public class Tegel {
+    
+    public int nummer;
+    public int aantalWormen;
+
+    public Tegel(int nummer, int aantalWormen) {
+        setNummer(nummer);
+        setAantalWormen(aantalWormen);
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public int getAantalWormen() {
+        return aantalWormen;
+    }
+
+    public final void setNummer(int nummer) {
+       if(nummer >= 21 && nummer <= 36){
+        this.nummer = nummer;
+       }else{
+           throw new IllegalArgumentException("Het nummer moet tussen 21 en 36 liggen.");
+       }
+    }
+
+    public final void setAantalWormen(int aantalWormen) {
+        if(aantalWormen >=1 && aantalWormen <= 4){
+            this.aantalWormen = aantalWormen;
+        }else{
+            throw new IllegalArgumentException("Het aantal wormen moet tussen 1 en 4 liggen.");
+        } 
+    }
+         
     
 }
