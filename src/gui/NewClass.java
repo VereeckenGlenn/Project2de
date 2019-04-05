1c457ade0d0dccbd88f8ef7b5b9ab13042158b3e
Lorenzo Van Der Pulst <lorenzovanderpulst@gmail.com>
2019-04-06 01:52:56 +0200

    Basis welkomscherm UI gemaakt

--- src/gui/NewClass.java
+++ src/gui/NewClass.java
@@ -1,59 +1,51 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class NewClass extends Application {
 
     Stage window;
+    Scene speelScherm;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         window = primaryStage;
-        window.setTitle("thenewboston - JavaFX");
-
-        //GridPane with 10px padding around edge
+        window.setTitle("Regenwormen");
         GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(8);
-        grid.setHgap(10);
+        grid.setAlignment(Pos.CENTER);
+        Label welkomLabel = new Label("Welkom bij het spel \"Regenwormen\"");
+        Button startButton = new Button("Klik hier om te beginnen");
+        startButton.setOnAction(e -> window.setScene(speelScherm));
+        VBox test = new VBox();
+        test.getChildren().addAll(welkomLabel, startButton);
 
-        //Name Label - constrains use (child, column, row)
-        Label nameLabel = new Label("Username:");
-        GridPane.setConstraints(nameLabel, 0, 0);
-
-        //Name Input
-        TextField nameInput = new TextField("Bucky");
-        GridPane.setConstraints(nameInput, 1, 0);
-
-        //Password Label
-        Label passLabel = new Label("Password:");
-        GridPane.setConstraints(passLabel, 0, 1);
-
-        //Password Input
-        TextField passInput = new TextField();
-        passInput.setPromptText("password");
-        GridPane.setConstraints(passInput, 1, 1);
-
-        //Login
-        Button loginButton = new Button("Log In");
-        GridPane.setConstraints(loginButton, 1, 2);
-
-        //Add everything to grid
-        grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton);
-
+        grid.getChildren().addAll(test);
+        test.setAlignment(Pos.CENTER);
         Scene scene = new Scene(grid, 300, 200);
         window.setScene(scene);
         window.show();
     }
+    
+    public void welkomScherm(){
+                GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        Label welkomLabel = new Label("Welkom bij het spel \"Regenwormen\"");
+        Button startButton = new Button("Klik hier om te beginnen");
+        VBox test = new VBox();
+        test.getChildren().addAll(welkomLabel, startButton);
 
-
+        grid.getChildren().addAll(test);
+        test.setAlignment(Pos.CENTER);
+        speelScherm = new Scene(grid, 300, 200);
+                
+    }
 }
